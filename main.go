07620cb891df18efea2088bfe3d7912c4d06a3b5
Alex Morton <mortonar01@gmail.com>
2017-10-15 10:45:05 -0400

    Directory spelling issue: sever => server.

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
     _ "github.com/jinzhu/gorm/dialects/postgres"
-    "github.com/mortonar/acs560_course_project/sever"
+    "github.com/mortonar/acs560_course_project/server"
 )
 
 func main() {
-    s := &sever.Server{}
+    s := &server.Server{}
     s.Start()
 }
--- server/client_connection.go
+++ server/client_connection.go
@@ -1,4 +1,4 @@
-package sever
+package server
 
 import (
     "net"
