d544d3e0cc3212fb1c6bee837585dda972bc1885
emmacki <31216583+emmacki@users.noreply.github.com>
2017-11-06 12:36:57 -0500

    Add XAML for Book Search
    Add XAML for Book Search

---
+++ client/BookTracker/BookSearchView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace BookTracker
+{
+    /// <summary>
+    /// Interaction logic for BookSearchView.xaml
+    /// </summary>
+    public partial class BookSearchView : UserControl
+    {
+        public BookSearchView()
+        {
+            InitializeComponent();
+        }
+    }
+}
