fcc61437effa771651367a7a9f6b1ced6e47cab6
emmacki <31216583+emmacki@users.noreply.github.com>
2017-11-11 20:52:51 -0500

    Add Author Title
    Add Author Title

---
+++ client/BookTracker/BookSearchModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookTracker
+{
+    public class BookSearchModel
+    {
+        private String _author;
+        private String _title;
+
+        public BookSearchModel()
+        {
+            _author = "George Orwell";
+            _title = "1984";
+        }
+
+        public String Author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+
+        public String Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+    }
+
+}
