3b189b715739b0e6b5265d7f41a81a931e29aed8
Drizzt159 <erebos159@gmail.com>
2017-12-01 10:47:09 -0500

    Adding Book View to lists and home page
    Updating all lists to have books as buttons to link to a book view. Updated home page to have the To Read list as well.

--- client/BookTracker/HomeViewModel.cs
+++ client/BookTracker/HomeViewModel.cs
@@ -1,4 +1,10 @@
 ﻿using BookTracker.HelperClasses;
+using BookTracker.Messaging.Requests;
+using BookTracker.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
 
 namespace BookTracker
 {
@@ -9,6 +15,45 @@ namespace BookTracker
             get { return "Home"; }
         }
 
-        public void Update() { }
+        public HomeViewModel()
+        {
+            Debug.WriteLine("Getting the To Read Book List.");
+            Update();
+        }
+
+        public void Update()
+        {
+
+            BookList bookList = new BookList
+            {
+                Name = Name
+            };
+
+            Base message = new Base
+            {
+                Action = "BookList",
+                Payload = bookList
+            };
+            try
+            {
+                string response = ServerProxy.Instance.sendRequest(message);
+                Debug.WriteLine("RESPONSE: " + response + "\n");
+                Messaging.Responses.Base responseMsg = JsonConvert.DeserializeObject<Messaging.Responses.Base>(response);
+                Debug.WriteLine("PAYLOD: " + responseMsg.Payload);
+                Messaging.Responses.BookSearch searchResponse = (responseMsg.Payload as JObject).ToObject<Messaging.Responses.BookSearch>();
+                _bookListModel = new BookListModel(searchResponse.Books);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Exception: " + e);
+            }
+        }
+
+        private BookListModel _bookListModel;
+        public BookListModel BookListModel
+        {
+            get { return _bookListModel; }
+            set { _bookListModel = value; }
+        }
     }
 }
